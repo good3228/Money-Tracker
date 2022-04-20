b5ad9022e44e7c09c7080822e84a7eb7467621d2
HaoyangHu <hu.haoy@northeastern.edu>
2022-04-20 02:34:03 -0400

    fetch all expense data

--- client/src/redux/slices/expenses/expensesSlices.js
+++ client/src/redux/slices/expenses/expensesSlices.js
@@ -34,9 +34,40 @@ async(payload,
 }
 );
 
+//  fetch all action
+export const fetchAllExpAction = createAsyncThunk("expense/fetchAll", 
+async(payload, 
+{rejectWithValue, getState, dispatch})=> {
+
+    const user_id = getState()?.users?.userAuth?._id;
+    const config = {
+        headers:{
+            "Content-Type": "application/json",
+        },
+    };
+    try {
+        const { data } = await axios.get("http://localhost:9000/userExpense/" + user_id, 
+        {
+            title: payload.title,
+            description: payload.description,
+            amount: payload.amount,
+            user: user_id
+        },
+        config
+        );
+        return data;
+    }catch(error) {
+        if(!error?.response) {
+            throw error;
+        }
+        return rejectWithValue(error?.response?.data);
+    }
+}
+);
+
 const expenseSlices = createSlice({
   name:'expenses',
-  initialState:{expense:["43", "53"]},
+  initialState:{},
   extraReducers:(builder) => {
     builder.addCase(createExpAction.pending, (state, action) => {
         state.loading = true;
@@ -52,7 +83,24 @@ const expenseSlices = createSlice({
       state.expenseCreated = action?.payload;
       state.appErr = undefined;
       state.serverErr = undefined;
-    })
+    });
+
+    //  fetch all Expenses
+    builder.addCase(fetchAllExpAction.pending, (state, action) => {
+      state.loading = true;
+    });
+  builder.addCase(fetchAllExpAction.fulfilled, (state, action) => {
+    state.loading = false;
+    state.expensesList = action?.payload;
+    state.appErr = undefined;
+    state.serverErr = undefined;
+    });
+  builder.addCase(fetchAllExpAction.rejected, (state, action) => {
+    state.loading = false;
+    state.expensesList = action?.payload;
+    state.appErr = undefined;
+    state.serverErr = undefined;
+    });
   },
 });
 
